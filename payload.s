f5a2b2d63eccd6b0351fa2ccce4fb536202e4902
Lukas Krickl <lukaskrickl@gmail.com>
2019-12-29 14:59:09 +0100

    Added save/load for actors

--- payload.s
+++ payload.s
@@ -13,6 +13,8 @@ constant INPUTS_LO($07C4)
 constant L_INPUT($15)
 constant CL_INPUT($11)
 constant CR_INPUT($10)
+constant CD_INPUT($12)
+constant CU_INPUT($13)
 constant START_INPUT($1C)
 
 constant INJECTED_RAM_HI($8000)
@@ -40,8 +42,14 @@ constant BALL_ROLL_VEL_X($F9D0)
 constant BALL_ROLL_VEL_Y($F9D8)
 constant BALL_ROLL_VEL_Z($F9DC)
 
-constant ACTOR_HEAP_START($802999F0)
-constant ACTOR_HEAP_SIZE($110)
+
+constant ACTOR_HEAP_START($8028FFFC)
+constant ACTOR_HEAP_END($803FFFFF) 
+// actor visible flag, search for this to find actors, search value is alwaus $20 bytes away from copy start
+constant ACTOR_SEARCH_VALUE($64) 
+constant ACTOR_OFFSET($3C) // offset from search value to copy start
+constant ACTOR_SIZE($110) // bytes to copy per actor 
+evaluate ACTOR_HEAP_SIZE((ACTOR_HEAP_END-ACTOR_HEAP_START+$04)/$04)
 
 constant ACTOR_HEAP_CLONE($80400000)
 
@@ -112,12 +120,6 @@ section_code:
 	lw t2, GLOVER_Z_LO(t3) // load Z position 
 	sw t2, Z_BAC(t1)
 
-	// test memcpy
-	la a0, ACTOR_HEAP_START
-	la a1, ACTOR_HEAP_CLONE
-	la a2, ACTOR_HEAP_SIZE
-	jal memcpy
-	nop
 not_CL:
 	
 	read_input(CR_INPUT)
@@ -151,20 +153,116 @@ not_CL:
 	sw t2, BALL_ROLL_VEL_X(t4)
 	sw t2, BALL_ROLL_VEL_Y(t4)
 	sw t2, BALL_ROLL_VEL_Z(t4)
-
-	// test memcpy
-	la a0, ACTOR_HEAP_CLONE
-	la a1, ACTOR_HEAP_START
-	la a2, ACTOR_HEAP_SIZE
-	jal memcpy
-	nop
 not_CR:
+	read_input(CU_INPUT)
+	blez t1, not_CU
+	nop
+
+	jal copy_actors
+	nop
+not_CU:
+
+	read_input(CD_INPUT)
+	blez t1, not_CD
+	nop 
+
+	jal restore_actors
+	nop
+not_CD:
 not_start:
 	// return
 	lui t3, {DATA_MASK} // set up direct load for return address
 	lw ra, return(t3) // load it 
 	jr ra // return
 	nop // need a nop after jr
+
+// finds all actors 
+// in the heap
+// based on a given search value ($64)
+// and memcpys them using the following format:
+// actor backup format:
+// 	4 bytes for original start address
+// 	ACTOR_SIZE of data
+//	List ends with a word of $00
+// uses A1 as the pointer to the next free backup heap location
+// each actor is ACTOR_SIZE bytes
+// the backup heap is locates at the start of exp pack memory
+copy_actors:
+	// set up all values we need
+	// avoid t1, t0, a0, a1 and a2 
+	// because memcpy uses those
+	la t4, ACTOR_HEAP_START
+	la t5, $01 // -1
+	la t6, ACTOR_SEARCH_VALUE
+	la t7, $04 // word size
+	la a1, ACTOR_HEAP_CLONE
+	la v0, {ACTOR_HEAP_SIZE} // loop counter
+
+	// search heap by the word 
+copy_actor_loop:
+	// next address
+	addu t4, t4, t7
+	lw t0, $00(t4) 
+
+	subu v0, v0, t5
+	beq v0, r0, copy_actor_done // if end address is reached quit 
+	nop 
+
+	// check search value 
+	bne t0, t6, copy_actor_loop
+	nop
+	// if loop falls through do a memcpy from a4 - $20 for ACTOR_SIZE 
+	// also write original address to A1 first and inc A1
+	// set up a0 src ptr
+	la t1, ACTOR_OFFSET // offset start
+	subu a0, t4, t1 // src ptr 
+
+	// store src ptr
+	sw a0, $00(a1)
+	addu a1, a1, t7 // next dest ptr
+
+	la a2, ACTOR_SIZE
+	// memcpy increments a1 so we do not have to do it here
+	// hold return address in v1 for now 
+	move v1, ra
+	jal memcpy
+	nop 
+	move ra, v1
+	j copy_actor_loop
+	nop
+copy_actor_done:
+
+	// end list with 1 word of $00
+	sw r0, $00(a1)
+	jr ra
+	nop
+
+// reverses actor backup
+// uses A0 as actor heap pointer
+restore_actors:
+	// start of clone list
+	la a0, ACTOR_HEAP_CLONE
+	la t5, $04 // +1 word
+	la t6, ACTOR_SIZE // +110
+
+restore_actor_loop:
+	// load address 
+	lw a1, $00(a0)
+	// if a1 is zero end of list 
+	beq a1, r0, restore_actor_done
+	nop
+
+	addu a0, a0, t5 // add 1 to src
+	move t7, ra 
+	la a2, ACTOR_SIZE
+	jal memcpy 
+	nop
+	move ra, t7 
+	j restore_actor_loop
+	nop
+restore_actor_done:
+	jr ra
+	nop
 
 // copies a block of memory from 
 // one address to another 
