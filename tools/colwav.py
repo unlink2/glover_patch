5e184fc22dad17b1a012cbdb0ae130fd80222188
Lukas Krickl <lukaskrickl@gmail.com>
2020-02-03 12:35:44 +0100

    Added collision export script that can operate on a ram dump

--- tools/colwav.py
+++ tools/colwav.py
@@ -14,9 +14,16 @@ import struct
 # to get a half word for size (amount of faces?) of collision
 # TODO there is lots of unknown data points in each polygon
 
+# data = raw collision data dumped from game
+# size = total size of collision data, if none given len(data) is used
+# node_size = size for each vertex
+# tri_offset = start of actual tri data in vertex node
+# returns an object, raw BE encoded floats, vertex list and a tuple of face string and face index
+def convert_to_obj(data, size=None, node_size=30, tri_offset=4, face_start_index=0):
+    if size is None:
+        size = len(data)
 
-def convert_to_obj(data):
-    format_str = '>' + ('f' * int(len(data)/4))
+    format_str = '>' + ('f' * int((size/4)))
     unpacked = struct.unpack_from(format_str, data)
 
     obj = []
@@ -24,23 +31,33 @@ def convert_to_obj(data):
     # points per face
     ppf = 3
 
-    for i in range(0, len(unpacked), 30):
+    for i in range(0, len(unpacked), node_size):
         scale = unpacked[i]
+        x = tri_offset+i
         # obj.append('v {0} {1} {2}'.format(unpacked[i+1], unpacked[i+2], unpacked[i+3]))
-        obj.append('v {0} {1} {2}'.format(unpacked[i+4], unpacked[i+5], unpacked[i+6]))
-        obj.append('v {0} {1} {2}'.format(unpacked[i+7], unpacked[i+8], unpacked[i+9]))
-        obj.append('v {0} {1} {2}'.format(unpacked[i+10], unpacked[i+11], unpacked[i+12]))
-        for j in range (i+4, i+13):
-            raw.append(struct.pack('>f', unpacked[j]))
+        try:
+            obj.append('v {0} {1} {2}'.format(unpacked[x], unpacked[x+1], unpacked[x+2]))
+            obj.append('v {0} {1} {2}'.format(unpacked[x+3], unpacked[x+4], unpacked[x+5]))
+            obj.append('v {0} {1} {2}'.format(unpacked[x+6], unpacked[x+7], unpacked[x+8]))
+            # vert.append('v {0} {1} {2}'.format(unpacked[x], unpacked[x+1], unpacked[x+2]))
+            # vert.append('v {0} {1} {2}'.format(unpacked[x+3], unpacked[x+4], unpacked[x+5]))
+            # vert.append('v {0} {1} {2}'.format(unpacked[x+6], unpacked[x+7], unpacked[x+8]))
+            for j in range (i+4, i+13):
+                raw.append(struct.pack('>f', unpacked[j]))
+        except IndexError:
+            break
         # obj.append('v {0} {1} {2}'.format(unpacked[i+13], unpacked[i+14], unpacked[i+15]))
         # obj.append('v {0} {1} {2}'.format(unpacked[i+16], unpacked[i+17], unpacked[i+18]))
         # obj.append('v {0} {1} {2}'.format(unpacked[i+19], unpacked[i+20], unpacked[i+21]))
 
-    for i in range(1, len(obj), ppf):
+    for i in range(face_start_index+1, face_start_index+len(obj), ppf):
         face = 'f'
+        fd = []
         for j in range(0, ppf):
             face += ' {0}'.format(i+j)
+            fd.append(i+j)
         obj.append(face)
+        # faces.append(face)
 
     return obj, raw
 
