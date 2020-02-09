eca1566231fdb7314feae5de0ab4fd0bf5de5a0e
Lukas Krickl <lukaskrickl@gmail.com>
2020-02-09 14:28:23 +0100

    Reduced polling rate for memwatch

--- src/include/memwatch.h
+++ src/include/memwatch.h
@@ -6,10 +6,12 @@
 
 #define WORDS_PER_PAGE 16
 #define BYTES_PER_LINE 4
+#define MEMWATCH_POLLING_RATE 4 // every N frames
 
 typedef struct memwatch {
     //  7th bit == 1 -> Watch enable flag
-    HWORD_T flags; // this really only needs to be 1 byte, but it is word for padding
+    BYTE_T flags; // this really only needs to be 1 byte, but it is word for padding
+    BYTE_T frame_counter; // used for polling rate
     HWORD_T cursor_pos; // cursor position, if FFFF it will select the address value
     WORD_T offset;
     BYTE_T *pstr; // string buffer, should have at least 18 bytes
@@ -20,6 +22,11 @@ typedef struct memwatch {
 memwatch* memwatch_from_addr(WORD_T *);
 
 void render_memwatch(memwatch *);
+
+/**
+ * Prepares memwatch for rendering
+ */
+void prepare_memwatch(memwatch *);
 
 void update_memwatch(memwatch *);
 
--- src/memwatch.c
+++ src/memwatch.c
@@ -5,14 +5,14 @@
 
 memwatch* memwatch_from_addr(WORD_T *paddress) {
     get_ptr(memwatch, newwatch, paddress, sizeof(memwatch));
-    get_ptr(BYTE_T, string_buffer, STRING_BUFFER, 20);
+    get_ptr(BYTE_T, string_buffer, SCREEN_BUFFER, 0x20*0x10);
     newwatch->pstr = string_buffer;
     newwatch->base_addr = 0x80000000;
     return newwatch;
 }
 
 void render_memwatch(memwatch *pmw) {
-    if ((pmw->flags & 0b1000000000000000) == 0) {
+    if ((pmw->flags & 0x80) == 0) {
         return;
     }
 
@@ -20,26 +20,17 @@ void render_memwatch(memwatch *pmw) {
 
     get_ptr(HWORD_T, pfont, FONT8X8, 0x4000);
 
+    // render strings
+    char *pstr = (char*)pmw->pstr;
+    gputsf(pstr, pframebuffer, 0x04, 0x04, pfont);
+    pstr += 0x10;
 
-    WORD_T *paddr = (WORD_T*)pmw->base_addr+(pmw->offset*WORDS_PER_PAGE);
 
-    if ((WORD_T)paddr < pmw->base_addr
-            || (WORD_T)paddr >= 0x80400000) {
-        pmw->offset = 0;
-        return;
-    }
-
-    word_to_hexstr((WORD_T)paddr, (char*)pmw->pstr);
-    gputsf((char*)pmw->pstr, pframebuffer, 0x04, 0x04, pfont);
-
-    // display 16 bytes on screen 2 words per line
     unsigned short start_x = 0x04;
     unsigned short start_y = 0x020;
-    for (int i = 0; i < WORDS_PER_PAGE; i++, start_y += CHAR_H+1) {
-        WORD_T *ptr = (WORD_T*)paddr+i;
-        word_to_hexstr(*ptr, (char*)pmw->pstr);
-        gputsf((char*)pmw->pstr, pframebuffer, start_x, start_y, pfont);
-
+    // display 16 bytes on screen 1 word per line
+    for (int i = 0; i < WORDS_PER_PAGE; i++, start_y += CHAR_H+1, pstr += 0x10) {
+        gputsf(pstr, pframebuffer, start_x, start_y, pfont);
     }
 
     int x_offset = (pmw->cursor_pos%BYTES_PER_LINE)*CHAR_W*2;
@@ -58,16 +49,41 @@ void render_memwatch(memwatch *pmw) {
             (WORD_T*)font8x8_basic, 0xF00F, 0x0000);
 }
 
+void prepare_memwatch(memwatch *pmw) {
+    WORD_T *paddr = (WORD_T*)pmw->base_addr+(pmw->offset*WORDS_PER_PAGE);
+
+    if ((WORD_T)paddr < pmw->base_addr
+            || (WORD_T)paddr >= 0x80400000) {
+        pmw->offset = 0;
+        return;
+    }
+    // string buffer
+    char *pstr = (char*)pmw->pstr;
+    word_to_hexstr((WORD_T)paddr, pstr);
+    pstr += 0x10;
+
+    // display 16 bytes on screen 1 word per line
+    for (int i = 0; i < WORDS_PER_PAGE; i++, pstr += 0x10) {
+        WORD_T *ptr = (WORD_T*)paddr+i;
+        word_to_hexstr(*ptr, pstr);
+    }
+}
+
 void update_memwatch(memwatch *pmw) {
     // toggle memwatch on start p2
     if (read_button(START_INPUT, CONTROLLER_2)
             && !read_button(START_INPUT, LAST_INPUT_2)) {
-        pmw->flags = pmw->flags ^ 0b1000000000000000;
+        pmw->flags = pmw->flags ^ 0x80;
     }
 
     // don't test these if memwatch is off
-    if ((pmw->flags & 0x8000) == 0) {
+    if ((pmw->flags & 0x80) == 0) {
         return;
+    }
+
+    if ((pmw->frame_counter % MEMWATCH_POLLING_RATE) == 0) {
+        // don't update every frame
+        prepare_memwatch(pmw);
     }
 
     // inc/dec memory offset
@@ -119,4 +135,6 @@ void update_memwatch(memwatch *pmw) {
             *paddr -= 1;
         }
     }
+
+    pmw->frame_counter++;
 }
