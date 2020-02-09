8333d8b212a7d2035f25dbc3e0223cfd3e760fb9
Lukas Krickl <lukaskrickl@gmail.com>
2020-02-09 09:03:18 +0100

    Fixed memory editor. Improved controls for memory editor

--- src/include/memwatch.h
+++ src/include/memwatch.h
@@ -10,7 +10,7 @@
 typedef struct memwatch {
     //  7th bit == 1 -> Watch enable flag
     HWORD_T flags; // this really only needs to be 1 byte, but it is word for padding
-    HWORD_T cursor_pos;
+    HWORD_T cursor_pos; // cursor position, if FFFF it will select the address value
     WORD_T offset;
     BYTE_T *pstr; // string buffer, should have at least 18 bytes
     WORD_T base_addr; // start address of ram
