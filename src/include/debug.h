7c11f2299bcd5d08f01d3e8491a8114a05aa5801
Lukas Krickl <lukaskrickl@gmail.com>
2020-03-05 09:32:34 +0100

    Added way to remove memorywatch

--- src/include/debug.h
+++ src/include/debug.h
@@ -9,37 +9,40 @@
 
 #include "typedefs.h"
 
-#define REG_CFG 0
-#define REG_STATUS 1
-#define REG_DMA_LEN 2
-#define REG_DMA_RAM_ADDR 3
-#define REG_MSG 4
-#define REG_DMA_CFG 5
-#define REG_SPI 6
-#define REG_SPI_CFG 7
-#define REG_KEY 8
-#define REG_SAV_CFG 9
-#define REG_SEC 10
-#define REG_VER 11
+#define KSEG1           0xA0000000
 
-#define REG_CFG_CNT 16
-#define REG_CFG_DAT 17
-#define REG_MAX_MSG 18
-#define REG_CRC 19
+#define REG_BASE        0x1F800000
+#define REG_FPG_CFG     0x0000
+#define REG_USB_CFG     0x0004
+#define REG_TIMER       0x000C
+#define REG_BOOT_CFG    0x0010
+#define REG_IOM_VER     0x0014
+#define REG_I2C_CMD     0x0018
+#define REG_I2C_DAT     0x001C
 
-#define ED_CFG_SDRAM_ON 0
-#define ED_CFG_SWAP 1
-#define ED_CFG_WR_MOD 2
-#define ED_CFG_WR_ADDR_MASK 3
+#define REG_FPG_DAT     0x0200
+#define REG_USB_DAT     0x0400
 
-#define ED_REGS(name) volatile u32* name = (u32*)0xA8040000;
+#define REG_SYS_CFG     0x8000
+#define REG_KEY         0x8004
+#define REG_DMA_STA     0x8008
+#define REG_DMA_ADDR    0x8008
+#define REG_DMA_LEN     0x800C
+#define REG_RTC_SET     0x8010
+#define REG_RAM_CFG     0x8018
+#define REG_IOM_CFG     0x801C
+#define REG_SDIO        0x8020
+#define REG_MCN_VER     0x8040
+#define REG_SDIO_ARD    0x8200
+#define REG_IOM_DAT     0x8400
+#define REG_DD_TBL      0x8800
+
+#define ED_REGS(name, reg) volatile u32* name = (u32*)(KSEG1 | REG_BASE | (reg));
 
 void evd_init();
 
-void evd_write_reg(u8, u16);
-u16 evd_read_reg(u8);
-void evd_write_msg(u8 dat);
+void evd_write_reg(u32, u32);
+u16 evd_read_reg(u32);
+void evd_write_msg(u32);
 
-void debug_print(u32 value);
-
-#endif 
+#endif
