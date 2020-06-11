0a8382570339699066f40a88d28faef0682bde92
Lukas Krickl <lukaskrickl@gmail.com>
2020-06-11 08:54:37 +0200

    Added a simple input minitor TODO needs menu item and better stick display

--- src/inputs.c
+++ src/inputs.c
@@ -9,6 +9,10 @@ BOOLEAN read_button(unsigned int button, WORD_T *pcontroller) {
     return (*pcontroller) >> button & 0x01;
 }
 
+char get_stick(unsigned char axis, WORD_T *pcontroller) {
+    return (*pcontroller) >> (axis*8);
+}
+
 void store_inputs(WORD_T *pcontroller, WORD_T *pdest) {
     (*pdest) = (*pcontroller);
 }
