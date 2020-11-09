fb6fbd734c97ef7da54899dc2a41e10b17d00e0d
Lukas Krickl <lukas@krickl.dev>
2020-11-09 20:37:36 +0100

    Updated to new version of mlisp

--- src/include/script.h
+++ src/include/script.h
@@ -1,21 +1,21 @@
 #ifndef __SCRIPT_H__
 #define __SCRIPT_H__
 
-#include "../../mlisp/src/include/mlisp.h"
+#include "../../mlisp/src/mlisp.h"
 
 #define VM_OUT_SIZE 128
 #define VM_CODE_SIZE 1024
 
 extern char vm_code_buffer[VM_CODE_SIZE];
 extern char out_buffer[VM_OUT_SIZE];
-extern lispvm_t vm;
+extern ml_lispvm_t vm;
 
-void set_script(lispvm_t *pvm, char *script, char *out_buffer);
+void set_script(ml_lispvm_t *pvm, char *script, char *out_buffer);
 
-void reset_vm(lispvm_t *pvm);
-void repl(lispvm_t *pvm, char *pinput, char *out_buffer);
+void reset_vm(ml_lispvm_t *pvm);
+void repl(ml_lispvm_t *pvm, char *pinput, char *out_buffer);
 
 
-char update_vm(lispvm_t *pvm, char *out_buffer);
+char update_vm(ml_lispvm_t *pvm, char *out_buffer);
 
-#endif 
+#endif
