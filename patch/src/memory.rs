3c0bfecb25d772e408f2bcf2c7d08a85def6f174
Lukas Krickl <lukas@krickl.dev>
2021-05-12 06:10:02 +0200

    Added menu tets

--- patch/src/memory.rs
+++ patch/src/memory.rs
@@ -3,3 +3,4 @@ pub const DISABLE_INPUT_TIMER: *mut u16 = 0x8028FBF4 as *mut u16;
 pub const DISABLE_PAUSE_FLAG: *mut u8 = 0x801EC748 as *mut u8;
 
 pub const CURRENT_MAP: *mut u8 = 0x801E7531 as *mut u8;
+pub const IS_PAUSED: *mut u8 = 0x801EC7D2 as *mut u8;
--- patch/src/update.rs
+++ patch/src/update.rs
@@ -2,14 +2,51 @@ extern crate ultrars;
 use self::ultrars::input::*;
 use self::ultrars::rdp::*;
 use self::ultrars::font::*;
+use self::ultrars::menu::*;
 use super::memory::*;
 
-pub struct InjectState {
+fn open_menu(entry: &mut Entry<Trigger>, trigger: Trigger) -> Option<usize> {
+    unsafe {
+        *IS_PAUSED = 1;
+    }
+    return None;
+}
+
+fn close_menu(entry: &mut Entry<Trigger>, trigger: Trigger) -> Option<usize> {
+    unsafe {
+        *IS_PAUSED = 0;
+    }
+    return None;
+}
+
+fn update_menu(entry: &mut Entry<Trigger>, trigger: Trigger) -> Option<usize> {
+    unsafe {
+        *IS_PAUSED = 1;
+    }
+    return None;
+}
+
+#[derive(Copy, Clone)]
+pub struct Trigger {
+    pub menu_active: bool
+}
+
+impl Trigger {
+    pub fn new() -> Self {
+        Self {
+            menu_active: false
+        }
+    }
+}
+
+pub struct InjectState<'a> {
     controller1: InputHandler,
     controller2: InputHandler,
     rdp_ctxt: RdpFontRendererContext,
     font: Font,
-    start_timer: u16
+    start_timer: u16,
+    trigger: Trigger,
+    menu: Menu<Trigger>,
 }
 
 impl InjectState {
@@ -21,8 +58,17 @@ impl InjectState {
             // TODO hard coded ptr is bad ok?
             rdp_ctxt: RdpFontRendererContext::new(
                 0x80529D40 as *mut u32,
-                786432),
-            font: Font::new(&FONT8X8_BASIC, 0x80525C30 as *mut u16, 0x000F, 0xFFFF)
+                196608),
+            font: Font::new(&FONT8X8_BASIC, 0x80525C30 as *mut u16, 0x000F, 0xFFFF),
+            trigger: Trigger::new(),
+            menu: Menu::new(10, 10,
+                Entry::new("open", no_op, open_menu),
+                Entry::new("close", no_op, close_menu),
+                Entry::new("back", no_op, no_op),
+                Entry::new("update", no_op, update_menu),
+                &[Entry::new("Test", no_op, no_op),
+                  Entry::new("Test", no_op, no_op),
+                  Entry::new("Test", no_op, no_op)])
         }
     }
 
@@ -32,16 +78,41 @@ impl InjectState {
             return;
         }
 
-        self.controller1.update();
-        self.controller2.update();
-		if self.controller1.read_button(Button::StartInput, false)
+        if self.controller1.read_button(Button::StartInput, false)
 			&& self.controller1.read_button(Button::BInput, false) {
 			self.level_select();
 		}
+
+        self.controller1.update();
+        self.controller2.update();
+
+        self.update_menu();
+    }
+
+    pub fn update_menu(&mut self) {
+        if self.controller1.read_button(Button::LInput, false)
+            && self.controller1.read_button(Button::RInput, false) {
+            self.menu.toggle(self.trigger);
+        }
+
+        // cursor
+        if self.menu.active {
+            if self.controller1.read_button(Button::DpadUp, true) {
+                self.menu.dec_cursor();
+            } else if self.controller1.read_button(Button::DpadDown, true) {
+                self.menu.inc_cursor();
+            }
+
+            if self.controller1.read_button(Button::AInput, true) {
+                self.menu.activate(self.trigger);
+            }
+        }
+
+        self.menu.update(self.trigger);
+
         self.rdp_ctxt.update();
 
-		// try rendering something :o
-        self.rdp_ctxt.puts("Hello", 20, 20, &self.font);
+        self.menu.draw(&mut self.rdp_ctxt, &mut self.font);
     }
 
     unsafe fn level_select(&self) {
