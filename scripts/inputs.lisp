f075307e3f8aca1e0646fdc41992f9aa6ea47321
Lukas Krickl <lukas@krickl.dev>
2024-06-25 05:38:55 +0200

    Removed issue template and unused script

--- scripts/inputs.lisp
+++
@@ -1,7 +0,0 @@
-; enables/disables input polling
-(defun 'toggleinputs '() '(pokei8 0x802AE8FC (^ (peeki8 0x802AE8FC) 0x4)))
-
-; toggles an a press
-(defun 'pressa '() '(pokei8 0x801FB280 (^ 0x80 (peeki8 0x801FB280))))
-
-
